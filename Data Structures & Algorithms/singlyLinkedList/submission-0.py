ddc6c71dfb3971458c49a9a462a52d3499672dda
Thi Hong Van Nguyen <van.nguyen.sp10@gmail.com>
2026-07-10 10:42:06 +0100

    Add: singlyLinkedList - submission-0

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-0.py
@@ -0,0 +1,62 @@
+class ListNode:
+    def __init__(self, val, next_node=None):
+        self.val = val
+        self.next = next_node
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = ListNode(-1)
+        self.tail = self.head
+    
+    # Return value of the i-th node
+    def get(self, index: int) -> int:
+        curr = self.head.next
+        i = 0 
+        while curr:
+            if i == index:
+                return curr.val
+            i += 1
+            curr = curr.next
+        return -1 # Index out of bounds or list is empty
+
+    # Insert a node with val at the head of the list
+    def insertHead(self, val: int) -> None:
+        new_node = ListNode(val)
+        # point the new node to the head of the current node
+        new_node.next = self.head.next
+        # make the head point to the new node
+        self.head.next = new_node
+        # update the tail if the node is empty
+        if not new_node.next:
+            self.tail = new_node
+
+    # Insert a node with val at the tail of the list
+    def insertTail(self, val: int) -> None:
+        new_node = ListNode(val)
+        self.tail.next = new_node
+        self.tail = self.tail.next
+
+    def remove(self, index: int) -> bool:
+        # Find the node just before the one to remove
+        i = 0 
+        curr = self.head
+        while i < index and curr:
+            i += 1
+            curr = curr.next
+
+        # Remove the node ahead of curr
+        if curr and curr.next:
+            if curr.next == self.tail:
+                self.tail = curr
+            curr.next = curr.next.next
+            return True
+        return False
+
+    def getValues(self) -> List[int]:
+        curr = self.head.next
+        res = []
+        while curr:
+            res.append(curr.val)
+            curr = curr.next
+        return res
