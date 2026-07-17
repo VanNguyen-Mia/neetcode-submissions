ce41bb0cb1dda5230d00b13f2be17d159c2d5df6
Thi Hong Van Nguyen <van.nguyen.sp10@gmail.com>
2026-07-17 22:47:19 +0100

    Add: design-linked-list - submission-0

---
+++ Data Structures & Algorithms/design-linked-list/submission-0.py
@@ -0,0 +1,73 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.head = ListNode(0)
+        self.tail = ListNode(0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+# Get the value at index node in the linked list.
+# If the index is invalid, return -1
+    def get(self, index: int) -> int:
+        cur = self.head.next
+        while cur and index > 0:
+            cur = cur.next
+            index -= 1
+            if cur and cur != self.tail and index == 0:
+                return cur.val
+        return -1
+
+# Add a node of value val before the first element of the linked list
+    def addAtHead(self, val: int) -> None:
+        node, next, prev = ListNode(val), self.head.next, self.head
+        prev.next = node
+        next.prev = node
+        node.next = next
+        node.prev = prev
+
+# Append a node of value val as the last element of the linked list
+    def addAtTail(self, val: int) -> None:
+        node, next, prev = ListNode(val), self.tail, self.tail.prev
+        prev.next = node
+        next.prev = node
+        node.next = next
+        node.prev = prev
+
+# Add a node of value val before the index-th node in the linked list
+    def addAtIndex(self, index: int, val: int) -> None:
+        cur = self.head.next
+        while cur and index > 0:
+            cur = cur.next
+            index -= 1
+        if cur and index == 0:
+            node, next, prev = ListNode(val), cur, cur.prev
+            prev.next = node
+            next.prev = node
+            node.next = next
+            node.prev = prev
+
+# Delete the index-th node in the index
+    def deleteAtIndex(self, index: int) -> None:
+        cur = self.head.next
+        while cur and index > 0:
+            cur = cur.next
+            index -= 1
+        if cur and cur != self.tail and index == 0:
+            next, prev = cur.next, cur.prev
+            next.prev = prev
+            prev.next = next
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
