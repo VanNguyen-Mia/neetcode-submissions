889f9abea938e86d5d7b11bca75dfcf350ccb26e
Thi Hong Van Nguyen <van.nguyen.sp10@gmail.com>
2026-08-04 22:59:33 +0100

    Add: mergeSort - submission-0

---
+++ Data Structures & Algorithms/mergeSort/submission-0.py
@@ -0,0 +1,50 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def mergeSort(self, pairs: List[Pair]) -> List[Pair]:
+        return self.mergeSortHelper(pairs, 0, len(pairs)-1)
+
+    def mergeSortHelper(self, pairs, s, e):
+        if e - s + 1 <= 1:
+            return pairs
+        
+        m = (s + e) // 2
+
+        self.mergeSortHelper(pairs, s, m)
+        self.mergeSortHelper(pairs, m+1, e)
+        self.merge(pairs, s, m, e)
+
+        return pairs
+    
+    def merge(self, arr, s, m, e):
+        left = arr[s:m+1]
+        right = arr[m+1: e+1]
+
+        # define pointers:
+        i = 0 #index for left array
+        j = 0 #index for right array
+        k = s #index for array
+
+        # Merge the two sorted halves
+        while i < len(left) and j < len(right):
+            if left[i].key <= right[j].key:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1 
+
+        # One of the halves will have elements remaining
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
